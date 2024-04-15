b1292c6bcc7a7e266a600dadd57432576918c5f8
alisonnbarbosa <alisonnbarbosaa@gmail.com>
2024-04-14 22:49:07 -0300

    v2.5

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-/*troca da imagem principal do produto*/
+/*troca a imagem principal do produto*/
 const imgm = document.getElementById('imgm')
-const btprevious = document.getElementById('btprevious')
-const btnext = document.getElementById('btnext')
-
-btprevious.addEventListener('click', ()=> button(-1))
-btnext.addEventListener('click', ()=> button(1))
+document.getElementById('btprevious').addEventListener('click', ()=> button(-1))
+document.getElementById('btnext').addEventListener('click', ()=> button(1))
 
 let num = 1
 
@@ -28,3 +25,30 @@ const navbar = document.getElementById('navbar')
 
 document.getElementById('menu').addEventListener('click', ()=> navbar.style.transform = 'translateX(0)')
 document.getElementById('close').addEventListener('click', ()=> navbar.style.transform = 'translateX(-100%)')
+
+/*button quantidade produto*/
+const outnum = document.getElementById('outnum')
+document.getElementById('plus').addEventListener('click', ()=> numproduto(1))
+document.getElementById('minus').addEventListener('click', ()=> numproduto(-1))
+
+let c = 0
+
+function numproduto(num){
+    c = c + num
+
+    if(c < 0){
+        c = 0
+        return
+    }
+
+    outnum.textContent = c
+}
+/*indicar a quantidade de produtos no cart*/
+const indicador = document.getElementById('indicador')
+document.getElementById('addcart').addEventListener('click', ()=> indicar())
+
+function indicar(){
+    if(c > 0){
+        indicador.textContent = c
+    }
+}
